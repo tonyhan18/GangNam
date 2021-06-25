1c1d0f7c22b8b49e472c28acbd52ea8327f95295
chahan <hgdst14@naver.com>
2021-06-25 21:18:17 +0900

    midterm

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chahan <hgdst14@naver.com>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/25 20:26:58 by chahan            #+#    #+#             */
+/*   Updated: 2021/06/25 20:29:53 by chahan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_memcpy(void *dst, const void *src, size_t n)
+{
+    unsigned char *mem_dst;
+    unsigned char *mem_src;
+    size_t i;
+
+    mem_dst = dst;
+    mem_src = (unsigned char *)src;
+    i = 0;
+    while (i ++ < n)
+        *(mem_dst++) = *(mem_src++);
+    return (dst);
+}
---
+++ libft/main.c
@@ -0,0 +1,49 @@
+# include <stdio.h>
+#include "libft.h"
+
+int     main(void)
+{
+    char a[5];
+
+    memset(a, 'a', sizeof(a));
+    for(unsigned int i = 0 ; i < 5; ++i)
+    {
+        printf("%c ", a[i]);
+    }
+    printf("\n");
+    ft_memset(a, 'b', sizeof(a));
+    for(unsigned int i = 0 ; i < 5; ++i)
+    {
+        printf("%c ", a[i]);
+    }
+    printf("\n-------\n");
+
+    bzero(a+2, 3);
+    for(unsigned int i = 0 ; i < 5; ++i)
+    {
+        printf("%c ", a[i]);
+    }
+    printf("\n");
+    ft_bzero(a+1, 4);
+    for(unsigned int i = 0 ; i < 5; ++i)
+    {
+        printf("%c ", a[i]);
+    }
+    printf("\n-------\n");
+
+    char b[5];
+    ft_memset(b,'b',sizeof(b));
+    memcpy(a,b,3);
+    for(unsigned int i = 0 ; i < 5; ++i)
+    {
+        printf("%c ", a[i]);
+    }
+    printf("\n");
+    ft_memcpy(a,b,5);
+    for(unsigned int i = 0 ; i < 5; ++i)
+    {
+        printf("%c ", a[i]);
+    }
+    printf("\n-------\n");
+    
+}
